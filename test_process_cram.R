cdb2189afce6902a6b5c4b5efb1066ab2b77e370
anguswg-ucsb <anguswatters@gmail.com>
2022-08-08 10:17:36 -0700

    resolving conflict file test_process_cram.R, hopefully

--- test_process_cram.R
+++ test_process_cram.R
@@ -14,35 +14,6 @@ base_folder       = "C:/Users/angus/Downloads/runs"
 (models = parse_directory(base_folder))
 
 cram1 <- process_cram(
-  base_year         = models$base_year[1],
-  model_version     = models$model_version[1],
-  base_folder       = models$base_folder[1],
-  return_wide       = TRUE
-)
-
-
-
-cram2 <- process_cram(
-  model_version     = "v0.466",
-  base_year         = 2025,
-  date_convert      = date_convert,
-  return_wide       = TRUE
-)
-
-cram3 <- process_cram2(
-  model_version     = "v0.463",
-  base_year         = 2025,
-  base_folder       = here::here("runs"),
-  base_model_folder = "/2025 v0.463",
-  date_convert      = date_convert,
-  return_wide       = TRUE
-)
-
-cram4 <- process_cram2(
-  model_version     = "v0.466",
-  base_year         = 2025,
-  base_folder       = here::here("runs"),
-  base_model_folder = "/2025 MoS Triggers v0.466",
-  date_convert      = date_convert,
-  return_wide       = TRUE
+  model_directory      = models[1,],
+  return_wide          = TRUE
 )
